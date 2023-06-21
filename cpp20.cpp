c11128c066fb813ea7a53ce84ae7f82f25b03e5c
Sarv <91484197+SarvT@users.noreply.github.com>
2023-06-21 21:55:10 +0530

    Create cpp20.cpp

---
+++ cpp20.cpp
@@ -0,0 +1,18 @@
+// Leet
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i: s){
+            if(i == '(' || i == '{' || i == '[') st.push(i);
+            else{
+                if(st.empty()) return false;
+                if(i == ')' && st.top() == '(') st.pop();
+                else if(i == ']' && st.top() == '[') st.pop();
+                else if(i == '}' && st.top() == '{') st.pop();
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
