cd568196d88dfc26f16ab3ebb5c43bd4c2549482
Sarv <91484197+SarvT@users.noreply.github.com>
2023-06-25 23:09:41 +0530

    Update cpp34.cpp

--- cpp34.cpp
+++ cpp34.cpp
@@ -17,3 +17,19 @@ public:
       return res;
     } 
 };
+
+// low performed
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+      vector<int> res(2, -1);
+      for(int i=0; i<nums.size(); i++){
+          if(nums[i] == target){
+              if(res[0] == -1) res[0] = i;
+              else res[1] = i;
+          }
+      }
+      if(res[0] != -1 && res[1] == -1) res[1]=res[0];
+      return res;
+    } 
+};
