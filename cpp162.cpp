7b4400a4babe7d213d20631c868e958e9ca1d95c
Sarv <91484197+SarvT@users.noreply.github.com>
2023-07-01 12:23:43 +0530

    Create cpp162.cpp

---
+++ cpp162.cpp
@@ -0,0 +1,34 @@
+// Leet
+// Not worked
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int i=0, j=nums.size()-1;
+        pair<int, int>maxi;
+        maxi.first=i;
+        maxi.second=nums[i];
+        if(nums[i]<nums[j]){
+            maxi.first=j;
+            maxi.second=nums[j];
+        }
+        i++;
+        j--;
+        // maxi.second=INT_MIN;
+        while(i<=j){
+            if(nums[i]>maxi.second){
+                maxi.first=i;
+                maxi.second=nums[i];
+                i++;
+            }
+            else if(nums[j]>maxi.second){
+                maxi.first=j;
+                maxi.second=nums[j];
+                j--;
+            } else{
+                i++;
+                j--;
+            }
+        }
+        return maxi.first;
+    }
+};
