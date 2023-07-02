318b863c7650b3e92a9eb15589d02dbd700f828c
Sarv <91484197+SarvT@users.noreply.github.com>
2023-07-02 19:35:10 +0530

    Update cpp452.cpp

--- cpp452.cpp
+++ cpp452.cpp
@@ -42,3 +42,32 @@ public:
         return cnt;
     }
 };
+
+
+
+
+
+
+bool comparei(vector<int>& a, vector<int>&b){
+    if(a[0]==b[0]) return a[1]<b[1];
+    return a[0]<b[0];
+}
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int n = points.size();
+        if(n<=1) return 1;
+        sort(points.begin(), points.end(), comparei);
+        int cnt=1, ans=points[0][1];
+        for(int i=1; i<n; i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            if(x>ans){
+            ans=y; 
+            cnt++;
+            }
+            else ans=min(ans, y);
+        }
+        return cnt;
+    }
+};
