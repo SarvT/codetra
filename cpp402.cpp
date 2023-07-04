e6bb50721a4b95fba97cd13829b7482fc2f24c01
Sarv <91484197+SarvT@users.noreply.github.com>
2023-07-04 15:39:10 +0530

    Create cpp402.cpp

---
+++ cpp402.cpp
@@ -0,0 +1,73 @@
+// Leet
+// Not worked
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        char l=num[0];
+        for(int i=0; i<num.size(); i++){
+            if(k<=0) break;
+            else{
+                k--;
+                num[i] = 'a';
+                cout << num[i];
+            }
+        }
+
+        string ans ="";
+        if()
+        for(char c: num){
+            if(c!='a'){
+                ans.push_back(c);
+            }
+        }
+        return ans;
+    }
+};
+
+// worked
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if(num.length() <= k) return "0";
+        
+        if(k == 0) return num;
+        
+        string res = "";
+        stack <char> s;
+        s.push(num[0]);
+        for(int i = 1; i<num.length(); ++i)
+        {
+            while(k > 0 && !s.empty() && num[i] < s.top())
+            {
+                --k;
+                s.pop();
+            }
+            
+            s.push(num[i]);
+            
+            if(s.size() == 1 && num[i] == '0')
+                s.pop();
+        }
+        
+        while(k && !s.empty())
+        {
+           --k;
+            s.pop();
+        }
+        
+        while(!s.empty())
+        {
+            res.push_back(s.top()); 
+            s.pop();
+        }
+        
+        reverse(res.begin(),res.end()); 
+        
+        if(res.length() == 0)
+            return "0";
+        
+        return res;
+        
+        
+    }
+};
