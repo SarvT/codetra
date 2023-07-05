6751aab1ad29d90cd83dc34a04faab823ab54c86
Sarv <91484197+SarvT@users.noreply.github.com>
2023-07-05 22:29:55 +0530

    Update Maximum good length.cpp

--- Maximum good length.cpp
+++ Maximum good length.cpp
@@ -1,3 +1,5 @@
+You are given a matrix A of size N*M. Any length 'L' can be called good length if there is at least one square matrix of size L*L in A such that all elements in that square matrix are greater than or equal to L. You need to output the maximum good length for given matrix.
+
 //{ Driver Code Starts
 //Initial Template for C++
 #include <bits/stdc++.h>
