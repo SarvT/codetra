bed2276892b57f4209eb3e466fc7b0b7ea4bec10
Sarv <91484197+SarvT@users.noreply.github.com>
2023-07-05 22:28:36 +0530

    Create Count beautiful strings.cpp

---
+++ Count beautiful strings.cpp
@@ -0,0 +1,3 @@
+Geek love strings having atmost one character whose frequency is odd and rest all (if exists) characters have even frequencies, he calls such strings as beautiful strings. Geek has a box containing n strings. You have to help him in counting number of pairs (i,j) (0<i<j<n-1) such that a string formed by concatenating string at index i with string at index j is a beautiful string. 
+
+Note: Strings consist of only lowercase English letters.
