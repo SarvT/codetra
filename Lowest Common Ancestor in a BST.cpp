b3d45147c86c1ac944dd2b657a4e722187423951
Sarvesh Dhnagar <91484197+SarvT@users.noreply.github.com>
2023-07-28 09:21:28 +0530

    Create Lowest Common Ancestor in a BST.cpp

---
+++ Lowest Common Ancestor in a BST.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+        Node* LCA(Node *root, int n1, int n2)
+        {
+            // code here
+            if(!root) return NULL;
+            if(root->data == n1 || root->data == n2) return root;
+            Node *left = LCA(root->left, n1, n2);
+            Node *right = LCA(root->right, n1, n2);
+            
+            if(left&&right) return root;
+            else if(left) return left;
+            return right;
+        }
+
+};
