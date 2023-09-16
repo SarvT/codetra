55fabfd5ed3be60ca39ec3caaa1cab1f82154d73
Sarvesh Dhnagar <91484197+SarvT@users.noreply.github.com>
2023-09-16 22:21:36 +0530

    Create Count number of hops.cpp

---
+++ Count number of hops.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int mod=1000000007;
+    long long solve(int n,vector<long long>&dp)
+    {
+        if(n<0)return 0LL;
+        if(n==0 || n==1)return 1LL;
+        if(dp[n]!=-1)return dp[n];
+        
+        long long a=solve(n-1,dp);
+        long long b=solve(n-2,dp);
+        long long c=solve(n-3,dp);
+        
+        return dp[n]=(a+b+c)%mod;
+    }
+    //Function to count the number of ways in which frog can reach the top.
+    long long countWays(int n)
+    {
+        
+        // your code here
+        vector<long long>dp(n+1,-1);
+        return solve(n,dp);
+        
+    }
+};
