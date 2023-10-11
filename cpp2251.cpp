6b7f36d90b25b81816f7480e72a6a0fac06debd1
Sarvesh Dhnagar <91484197+SarvT@users.noreply.github.com>
2023-10-11 22:20:17 +0530

    Create cpp2251.cpp

---
+++ cpp2251.cpp
@@ -0,0 +1,46 @@
+// not worked
+class Solution {
+public:
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<int> ans;
+        unordered_map<int, int>mp;
+        
+        for(auto it:flowers){
+            for(int i=0; i<2; i++){
+                mp[it[i]]++;
+            }
+
+        }
+        for(int i=0; i<people.size(); i++){
+            ans.emplace_back(mp[people[i]]);
+        }
+    }
+};
+
+
+
+// worked
+class Solution {
+public:
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<int> starts;
+        vector<int> ends;
+        
+        for (vector<int>& flower : flowers) {
+            starts.push_back(flower[0]);
+            ends.push_back(flower[1] + 1);
+        }
+        
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+        vector<int> ans;
+        
+        for (int person : people) {
+            int i = upper_bound(starts.begin(), starts.end(), person) - starts.begin();
+            int j = upper_bound(ends.begin(), ends.end(), person) - ends.begin();
+            ans.push_back(i - j);
+        }
+        
+        return ans;
+    }
+};
