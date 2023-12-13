666d915493b1453f27a6415b0957f35994a7cae2
Sarvesh Dhangar <91484197+SarvT@users.noreply.github.com>
2023-12-13 22:03:40 +0530

    Create Consecutive 1's not allowed.cpp

---
+++ Consecutive 1's not allowed.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+	long long countStrings(int n) {
+	    // code here
+	    long long dp[n+1];
+        dp[1] = 2;
+        dp[0] = 1;
+        int mod = (int)1e9+7;
+        for(int i= 2; i<=n; i++) dp[i] = (dp[i-1]+dp[i-2])%mod;
+        return dp[n]%mod;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
