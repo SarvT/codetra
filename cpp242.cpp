cc3b572b99a202b0f807f81c56c0aa9ecdd18f17
Sarvesh Dhangar <91484197+SarvT@users.noreply.github.com>
2023-12-16 22:33:02 +0530

    Create cpp242.cpp

---
+++ cpp242.cpp
@@ -0,0 +1,20 @@
+int map[26] = {0};
+if (s.size() != t.size()) {
+          return false;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            int idx = s[i] - 'a';
+            map[idx]++;
+        }
+
+        for (int j = 0; j < t.size(); j++) {
+            int idx = t[j] - 'a';
+            map[idx]--;
+        }
+
+        for (int k = 0; k < 26; k++) {
+            if (map[k] != 0)
+                return false;
+        }
+        return true;
