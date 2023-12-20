a8bbbffdfd4b882741b2c72edb866a45d98ead70
Sarvesh Dhangar <91484197+SarvT@users.noreply.github.com>
2023-12-20 15:42:00 +0530

    Create Modified Game of Nim.cpp

---
+++ Modified Game of Nim.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        // code here
+        int w = 1;
+        if (n%2==1) w=2;
+        for(int i=1; i<n; i++) A[0]^=A[i];
+        if (A[0]==0) w=1;
+        return w;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.findWinner(n, A)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
