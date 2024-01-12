2d8a214df735e8c0434eaae583e2df63d22f503f
Sarvesh <91484197+SarvT@users.noreply.github.com>
2024-01-12 22:30:36 +0530

    Create cpp2572.cpp

---
+++ cpp2572.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+int gcd(int a,int b){
+        return b==0 ?  a :  gcd(b,a%b);
+    }
+    int primes[10]={2,3,5,7,11,13,17,19,23,29};
+    int squares[4]={4,9,16,25};
+
+    int mod=1e9+7;
+    
+    bool poss(int sol,vector<int>&vis){
+        for(int i=0;i<10;i++)if(sol%primes[i]==0 && vis[i]>0)return false;
+        for(int i=0;i<4;i++)if(sol%squares[i]==0)return false;
+        return true;
+    }
+
+    int solve(int n,vector<int>& nums,vector<int>&vis,vector<vector<int>>&dp){
+        if(!n)return 1;
+        int cnt=0;
+        for(int i=0;i<10;i++)if(vis[i]==1)cnt+=(1<<i);
+        
+        if(dp[n][cnt]!=-1)return dp[n][cnt];
+
+        int ans=solve(n-1,nums,vis,dp);
+        
+        if(poss(nums[n-1],vis)){
+            for(int i=0;i<10;i++)if(nums[n-1]%primes[i]==0)vis[i]=1;
+            long long sol=((long long)ans%mod + (solve(n-1,nums,vis,dp))%mod)%mod;
+            ans=sol;
+            for(int i=0;i<10;i++)if(nums[n-1]%primes[i]==0)vis[i]=0;
+        }
+        
+        return dp[n][cnt]=ans;
+    }
+    int squareFreeSubsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>vis(11,0);
+        vector<vector<int>>dp(n+1,vector<int>(1<<10,-1));
+        int ans=solve(n,nums,vis,dp);
+        if(ans-1==-1)return 1000000006;
+        return ans-1;
+    }
+};
