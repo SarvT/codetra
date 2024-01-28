e7c382b66725e801f022eb52e4cee9f7d56471ac
Sarvesh <91484197+SarvT@users.noreply.github.com>
2024-01-28 19:33:13 +0530

    Create cpp1074.cpp

---
+++ cpp1074.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+     void printMat(vector<vector<int>> mat){
+         for(auto &row : mat){
+             for(auto val : row)cout<<val<<" ";
+             cout<<endl;
+         }
+         cout<<endl;
+     }    
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size(), cols = matrix[0].size();
+        for(auto &row : matrix){
+            for(int col = 1; col < cols; col++)row[col] += row[col-1];
+        }
+        int cnt = 0;
+        for(int left = 0; left < cols; left++){
+            for(int right = left; right < cols; right++){
+                int currPrefSum = 0;
+                unordered_map<int, int> sumFreq;
+                sumFreq[0]++;
+                for(int r = 0; r < rows; r++){
+                    int prefSumCurrRow = matrix[r][right] - ((left - 1 > -1) ? matrix[r][left - 1] : 0);
+                    currPrefSum += prefSumCurrRow;
+                    if(sumFreq.count(currPrefSum-target))cnt += sumFreq[currPrefSum-target];
+                    sumFreq[currPrefSum]++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
