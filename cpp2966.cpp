63bc549139180212bbdf6a2e41445b77a50d98a3
Sarvesh <91484197+SarvT@users.noreply.github.com>
2024-02-01 23:13:16 +0530

    Create cpp2966.cpp

---
+++ cpp2966.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+        int n = nums.size();
+        for(int i =0;i<n-2;i+=3){
+            if(nums[i+2]-nums[i]<=k)res.push_back({nums[i],nums[i+1],nums[i+2]});
+            else return {};
+        }
+        return res;
+    }
+};
