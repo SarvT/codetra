5c00ee53bcd982d82cada5bc55fc956e6a7787bf
Sarvesh <91484197+SarvT@users.noreply.github.com>
2024-04-22 20:26:34 +0530

    Create Row with minimum number of 1's.cpp

---
+++ Row with minimum number of 1's.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minRow(int n, int m, vector<vector<int>> a) {
+        // code here
+        int min=INT_MAX, row;
+        for(int i=0;i<n;i++){
+            int tmp=0;
+            for(int j=0;j<m;j++)if(a[i][j]==1)tmp++;
+          
+             if(tmp < min){
+                  min=tmp;
+                  row=i+1;
+             }
+        }
+        return row;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> A(N, vector<int>(M));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> A[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.minRow(N, M, A) << "\n";
+    }
+}
+// } Driver Code Ends
