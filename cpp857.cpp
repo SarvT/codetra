bcd4dad2dc4a15f188f92365d6628a2c76d55a04
Sarvesh <91484197+SarvT@users.noreply.github.com>
2024-05-11 21:53:34 +0530

    Create cpp857.cpp

---
+++ cpp857.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        vector<vector<double>> workers(quality.size(), vector<double>(2));
+
+        for (int i = 0; i < quality.size(); i++) {
+            workers[i][0] = static_cast<double>(wage[i]) / quality[i];
+            workers[i][1] = quality[i];
+        }
+
+        sort(workers.begin(), workers.end(), [](const vector<double>& a, const vector<double>& b) {
+            return a[0] < b[0];
+        });
+
+        priority_queue<int> maxHeap;
+        int qualitySum = 0;
+        double result = numeric_limits<double>::max();
+        for (const auto& worker : workers) {
+            double ratio = worker[0];
+            int q = static_cast<int>(worker[1]);
+
+            qualitySum += q;
+            maxHeap.push(q);
+
+            if (maxHeap.size() > k) {
+                qualitySum -= maxHeap.top();
+                maxHeap.pop();
+            }
+
+            if (maxHeap.size() == k) result = min(result, ratio * qualitySum);
+        }
+
+        return result;
+    }
+};
