5b10ebf627cbd226477f6ac444fa71cd798a58de
Sarvesh D. <91484197+SarvT@users.noreply.github.com>
2024-10-15 23:25:43 +0530

    Create Subarray range with given sum.java

---
+++ Subarray range with given sum.java
@@ -0,0 +1,138 @@
+//TLE
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to count the number of subarrays which adds to the given sum.
+    static int subArraySum(int arr[], int tar) {
+        // add your code here
+        int cnt=0;
+        // for(int i=0; i<arr.length; i++){
+            // for(int j=i; j<arr.length; j++){
+                // sum+=(arr[j]);
+                // if(sum==tar) cnt++;
+            // }
+        // }
+        
+            int sum=0, i=0, j=0;
+        while(j<arr.length){
+            
+                // sum+=(arr[j]);
+                if(sum==tar) cnt++;
+                else if(sum>tar){
+                    i++;
+                    sum-=arr[i];
+                } else if(sum<tar){
+                    j++;
+                    sum+=tar;
+                }
+            
+        }
+        return cnt;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int tar = Integer.parseInt(br.readLine());
+            Solution obj = new Solution();
+            int res = obj.subArraySum(arr, tar);
+
+            System.out.println(res);
+            // System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+// worked
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to count the number of subarrays which adds to the given sum.
+    static int subArraySum(int arr[], int tar) {
+        // add your code here
+        HashMap<Integer,Integer>mp = new HashMap<>();
+        int sum = 0, sol = 0;
+        mp.put(0,1);
+        for(int i=0;i<arr.length;i++){
+            sum = sum + arr[i];
+            if(mp.containsKey(sum-tar))sol = sol + mp.get(sum-tar);
+            mp.put(sum,mp.getOrDefault(sum,0)+1);
+        }
+        return sol;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int tar = Integer.parseInt(br.readLine());
+            Solution obj = new Solution();
+            int res = obj.subArraySum(arr, tar);
+
+            System.out.println(res);
+            // System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
