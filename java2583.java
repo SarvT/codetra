93119d1eb150c21927e9acaa4b7a2dfcfe654962
Sarvesh D. <91484197+SarvT@users.noreply.github.com>
2024-10-22 21:26:13 +0530

    Create java2583.java

---
+++ java2583.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        Queue<TreeNode> bfsQueue = new LinkedList<>();
+        bfsQueue.add(root);
+        while (!bfsQueue.isEmpty()) {
+            int size = bfsQueue.size();
+            long sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode poppedNode = bfsQueue.remove();
+                sum += poppedNode.val;
+                if (poppedNode.left != null) bfsQueue.add(poppedNode.left);
+                if (poppedNode.right != null) bfsQueue.add(poppedNode.right);
+            }
+            pq.add(sum);
+            if (pq.size() > k) pq.remove();
+        }
+        if (pq.size() < k) return -1;
+        return pq.peek();
+    }
+}
