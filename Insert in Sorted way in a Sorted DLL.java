e9893fba57a1427b8e88812b166410550d276b4f
Sarvesh D. <91484197+SarvT@users.noreply.github.com>
2024-10-31 22:12:02 +0530

    Create Insert in Sorted way in a Sorted DLL.java

---
+++ Insert in Sorted way in a Sorted DLL.java
@@ -0,0 +1,207 @@
+//TLE
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node prev, next;
+
+    Node(int data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Ignore the newline character after t
+
+        while (t-- > 0) {
+            String input = sc.nextLine(); // Read the entire line for the array elements
+            String[] values = input.split(" ");
+
+            Node head = null, tail = null;
+            for (String value : values) {
+                int x = Integer.parseInt(value);
+                if (head == null) {
+                    head = new Node(x);
+                    tail = head;
+                } else {
+                    tail.next = new Node(x);
+                    tail.next.prev = tail;
+                    tail = tail.next;
+                }
+            }
+
+            int valueToInsert = sc.nextInt();
+            if (sc.hasNextLine()) {
+                sc.nextLine(); // Ignore the newline character after the value
+            }
+
+            Solution obj = new Solution();
+            head = obj.sortedInsert(head, valueToInsert);
+            printList(head);
+        
+System.out.println("~");
+}
+
+        sc.close();
+    }
+
+    public static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/*class of the node of the DLL is as
+/*
+class Node {
+    int data;
+    Node prev, next;
+    Node(int data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+}
+*/
+class Solution {
+    public Node sortedInsert(Node head, int x) {
+        // add your code here
+        Node tmp = head, prv = null;
+        while(tmp != null){
+            if(tmp.data>x && prv!= null && prv.data<=x){
+                Node newNode = new Node(x);
+                prv.next = newNode;
+                newNode.prev = tmp.prev;
+                newNode.next = tmp;
+                tmp.prev = newNode;
+            } else if(prv==null && tmp.data>x){
+                
+                Node newNode = new Node(x);
+                newNode.prev = null;
+                newNode.next = tmp;
+                tmp.prev = newNode;
+                head = newNode;
+                break;
+            } else if(tmp.next == null && tmp.data<=x){
+                Node newNode = new Node(x);
+                newNode.prev = tmp;
+                newNode.next = null;
+                tmp.next = newNode;
+                
+            }
+            prv = tmp;
+            tmp = tmp.next;
+        }
+        return head;
+    }
+}
+
+
+// worked
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node prev, next;
+
+    Node(int data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Ignore the newline character after t
+
+        while (t-- > 0) {
+            String input = sc.nextLine(); // Read the entire line for the array elements
+            String[] values = input.split(" ");
+
+            Node head = null, tail = null;
+            for (String value : values) {
+                int x = Integer.parseInt(value);
+                if (head == null) {
+                    head = new Node(x);
+                    tail = head;
+                } else {
+                    tail.next = new Node(x);
+                    tail.next.prev = tail;
+                    tail = tail.next;
+                }
+            }
+
+            int valueToInsert = sc.nextInt();
+            if (sc.hasNextLine()) {
+                sc.nextLine(); // Ignore the newline character after the value
+            }
+
+            Solution obj = new Solution();
+            head = obj.sortedInsert(head, valueToInsert);
+            printList(head);
+        
+System.out.println("~");
+}
+
+        sc.close();
+    }
+
+    public static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/*class of the node of the DLL is as
+/*
+class Node {
+    int data;
+    Node prev, next;
+    Node(int data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+}
+*/
+class Solution {
+    public Node sortedInsert(Node head, int x) {
+        // add your code here
+        Node newNode = new Node(x);
+        if (head == null || head.data >= x) {
+            newNode.next = head;
+            if (head != null) head.prev = newNode;
+            return newNode;
+        }
+
+        Node current = head;
+        while (current.next != null && current.next.data < x) current = current.next;
+
+        newNode.next = current.next;
+        if (current.next != null) current.next.prev = newNode;
+        current.next = newNode;
+        newNode.prev = current;
+
+        return head;
+    }
+}
