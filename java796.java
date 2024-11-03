d7335e96714de963c6b4b30ab5dc67be24166dc3
Sarvesh D. <91484197+SarvT@users.noreply.github.com>
2024-11-03 23:40:46 +0530

    Create java796.java

---
+++ java796.java
@@ -0,0 +1,8 @@
+//charm
+class Solution {
+    public boolean rotateString(String s, String goal) {
+       if (s.length() != goal.length()) return false;
+        String str = s + s;
+        return str.contains(goal); 
+    }
+}
