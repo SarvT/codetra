e685778516c4e699d2af639de029128535bf7f2d
Sarvesh D. <91484197+SarvT@users.noreply.github.com>
2025-05-01 23:15:59 +0530

    Create _Pascal Triangle.cpp

---
+++ _Pascal Triangle.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    vector<int> nthRowOfPascalTriangle(int n) {
+        // code here
+        int rowIndex=n-1;
+        vector<int>row(rowIndex+1,1);
+        for (int i=1;i<rowIndex;++i) {
+            row[i]=(long)row[i-1]*(rowIndex-i+1)/i;
+        }
+        return row;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+void printAns(vector<int> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
